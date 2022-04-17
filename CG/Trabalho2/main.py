14eebc2b546e711ea15005c1baa48eeb9a139fbc
ldunhao <ldunhao@hotmail.com>
2022-04-17 16:09:31 -0300

    Finalizando Trabalho 2 de Computacao Grafica

---
+++ CG/Trabalho2/main.py
@@ -0,0 +1,8 @@
+# Trabalho 2 de Computação Gráfica
+# Alunos: Eduardo Arthur e Lucas Dunhão
+
+import numpy as np
+import afimTransform
+
+po = np.array([[-0.5,-0.5,1],[0.5,-0.5,1],[0.5,0.5,1],[-0.5,0.5,1]])
+afimTransform.startAfimTransformation(po)
