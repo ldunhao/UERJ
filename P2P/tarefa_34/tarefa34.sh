a504255efb89cd88b5754bef4fafc08a0355a8d9
ows.lucas <ldunhao@hotmail.com>
2022-09-26 23:25:29 -0300

    tarefa 3.4

--- P2P/tarefa_34/tarefa34.sh
+++ P2P/tarefa_34/tarefa34.sh
@@ -2,10 +2,6 @@ rm -rf /tmp/myhost
 rm -rf /tmp/usuario1
 rm -rf /tmp/usuario2
 
-gnome-terminal -- freechains-host start /tmp/myhost
-gnome-terminal -- freechains-host --port=8081 start /tmp/usuario1
-gnome-terminal -- freechains-host --port=8082 start /tmp/usuario2
-
 
 # HASHs #############
 pioneiroPub=0D340D7547805C73CD6B96BA4F41814428D0EB5A5D79538C76173E87C867C5DE
@@ -20,21 +16,57 @@ trollPvt=71B1C4ED9E657DD48AEBCEEE1889833B05D636B8CEDD7C8981DBDD1B7790D12415E557C
 
 
 echo "Simulação dia 1"
-currentDate=`date +%s`
-declare -i seconds=60
-declare -i minutes=60
-declare -i hours=24
-nextTimestamp=$(($currentDate + $seconds*$minutes*$hours*1))
+
+hostPioneiro=8081
+hostNewbie=8082
+hostTroll=8083
+
+gnome-terminal -- freechains-host --port=$hostPioneiro start /tmp/myhost
+gnome-terminal -- freechains-host --port=$hostNewbie start /tmp/usuario1
+gnome-terminal -- freechains-host --port=$hostTroll start /tmp/usuario2
+
+sleep 1s
 
 echo "Pioneiro entrando no fórum"
-freechains chains join '#forum' $pioneiroPub
+freechains --host=localhost:$hostPioneiro chains join '#forum' $pioneiroPub
 
 echo "Newbie entrando no fórum"
-freechains --host=localhost:8081 chains join '#forum' $newbiePub
+freechains --host=localhost:$hostNewbie chains join '#forum' $pioneiroPub
 
 echo "Troll entrando no fórum"
-freechains --host=localhost:8082 chains join '#forum' $trollPub
+freechains --host=localhost:$hostTroll chains join '#forum' $pioneiroPub
 
+syncHost () {
+    echo "Sincronizando pioneiro com o newbie"
+    freechains --host=localhost:$hostPioneiro peer localhost:$hostNewbie recv '#forum'
+    echo "Sincronizando pioneiro com o troll"
+    freechains --host=localhost:$hostPioneiro peer localhost:$hostTroll recv '#forum'
+
+    sleep 1s
+
+    echo "Sincronizando newbie com o pioneiro"
+    freechains --host=localhost:$hostNewbie peer localhost:$hostPioneiro recv '#forum'
+    echo "Sincronizando newbie com o troll"
+    freechains --host=localhost:$hostNewbie peer localhost:$hostTroll recv '#forum'
+
+    sleep 1s
+
+    echo "Sincronizando troll com o pioneiro"
+    freechains --host=localhost:$hostTroll peer localhost:$hostPioneiro recv '#forum'
+    echo "Sincronizando troll com o newbie"
+    freechains --host=localhost:$hostTroll peer localhost:$hostNewbie recv '#forum'
+}
+
+getReps () {
+    echo "Reputacao pioneiro"
+    echo `freechains --host=localhost:$hostPioneiro chain '#forum' reps $pioneiroPub`
+
+    echo "Reputacao newbie"
+    echo `freechains --host=localhost:$hostNewbie chain '#forum' reps $newbiePub`
+
+    echo "Reputacao troll"
+    echo `freechains --host=localhost:$hostTroll chain '#forum' reps $trollPub`
+}
 
 for i in {1..91..15}
 do
@@ -47,39 +79,42 @@ do
     nextTimestamp=$(($currentDate + $seconds*$minutes*$hours*$i))
 
     echo "Setando timestamp do pioneiro para daqui a $i dias"
-    freechains-host now $nextTimestamp
+    freechains-host --port=$hostPioneiro now $nextTimestamp
 
     echo "Setando timestamp do newbie para daqui a $i dias"
-    freechains-host --host=localhost:8081 now $nextTimestamp
+    freechains-host --port=$hostNewbie now $nextTimestamp
 
     echo "Setando timestamp do troll para daqui a $i dias"
-    freechains-host --host=localhost:8082 now $nextTimestamp
+    freechains-host --port=$hostTroll now $nextTimestamp
 
     echo "Post do dia $i do pioneiro"
-    postPioneiro=`freechains chain '#forum' post inline "Mensagem do pioneiro dia $i" --sign=$pioneiroPvt`
+    postPioneiro=`freechains --host=localhost:$hostPioneiro chain '#forum' post inline "Mensagem do pioneiro dia $i" --sign=$pioneiroPvt`
 
     echo "Post do dia $i do newbie"
-    postNewbie=`freechains --host=localhost:8081 chain '#forum' post inline "Mensagem do newbie dia $i" --sign=$newbiePvt`
+    postNewbie=`freechains --host=localhost:$hostNewbie chain '#forum' post inline "Mensagem do newbie dia $i" --sign=$newbiePvt`
 
     echo "Post do dia $i do troll"
-    postTroll=`freechains --host=localhost:8082 chain '#forum' post inline "Mensagem do troll dia $i" --sign=$trollPvt`
+    postTroll=`freechains --host=localhost:$hostTroll chain '#forum' post inline "Mensagem do troll dia $i" --sign=$trollPvt`
 
-    # if [ $(($i % 2)) = 0 ]; then
+    syncHost
+
+    if [ $(($i % 2)) = 0 ]; then
         echo "Curtindo o post do pioneiro"
-        freechains chain '#forum' like $postPioneiro --sign=$pioneiroPvt
-    # else
+        freechains chain '#forum' --port=$hostNewbie like $postPioneiro --sign=$newbiePvt
+    else
         echo "Curtindo o post do newbie"
-        freechains chain '#forum' like $postNewbie --sign=$pioneiroPvt
-    # fi
+        freechains chain '#forum' --port=$hostPioneiro like $postNewbie --sign=$pioneiroPvt
+    fi
 
-    # if [ $i -lt 70 ]; then
+    if [ $i -lt 31 ]; then
         echo "Curtindo o post do troll"
-        freechains chain '#forum' like $postTroll --sign=$pioneiroPvt
-    # else
-    #     echo "Dando dislike no post do troll"
-    #     freechains chain '#forum' dislike $postTroll --sign=$pioneiroPvt
-    # fi
+        freechains chain '#forum' --port=$hostPioneiro like $postTroll --sign=$pioneiroPvt
+    else
+        echo "Dando dislike no post do troll"
+        freechains chain '#forum' --port=$hostPioneiro dislike $postTroll --sign=$pioneiroPvt
+    fi
 
+    getReps
     sleep 2s
 done
 
